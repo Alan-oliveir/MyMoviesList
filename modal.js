49bc55d9f5bd7f8b385586eb0f079a92a130bbd3
Alan-oliveir <alandeog@poli.ufrj.br>
2023-06-06 20:26:54 -0300

    finalized modal tansitons

--- modal.js
+++ modal.js
@@ -2,3 +2,9 @@ const overlay = document.getElementById("modal-overlay");
 const background = document.getElementById("modal-background");
 
 overlay.classList.add("open");
+
+function backgroundClickHandler() {
+  overlay.classList.remove("open");
+}
+
+background.addEventListener("click", backgroundClickHandler);
