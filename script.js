46d995b59de83178880e2a85807259f28c281e43
Alan-oliveir <alandeog@poli.ufrj.br>
2023-06-06 22:18:29 -0300

    tratament of users errors

--- script.js
+++ script.js
@@ -4,15 +4,32 @@ const movieName = document.getElementById("movie-name");
 const movieYear = document.getElementById("movie-year");
 
 async function searchButtonClickHandler() {
-  let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieName.value
-    .split(" ")
-    .join("+")}&${movieYear.value}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log("data: ", data);
-  overlay.classList.add("open");
-  // console.log(movieName.value.split(" ").join("+"));
-  // console.log("Ano: ", movieYear.value);
+  try {
+    let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieNameParameterGenerator()}&y=${movieYearParameterGenerator}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("data: ", data);
+    overlay.classList.add("open");
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  function movieNameParameterGenerator() {
+    if (movieName.value === "") {
+      throw new Error("O nome do filme deve ser informado");
+    }
+    return movieName.value.split(" ").join("+");
+  }
+
+  function movieYearParameterGenerator() {
+    if (movieYear.value === "") {
+      return "";
+    }
+    if (movieYear.value.lenght !== 4 || Number.isNaN(Number(movieYear.value))) {
+      throw new Error("Ano do filme inválido.");
+    }
+    return `&y=${movieYear.value}`;
+  }
 }
 
 searchButton.addEventListener("click", searchButtonClickHandler);
